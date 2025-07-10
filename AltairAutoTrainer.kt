7e484aaad8c2923532c3a31ca7d56473fecaa2dc
DarkKnight517 <130931785+DarkKnight517@users.noreply.github.com>
2025-07-10 08:46:58 +0000

    Initial commit with AltairAutoTrainer and patch

---
+++ AltairAutoTrainer.kt
@@ -0,0 +1,18 @@
+package com.sultan.altair.trainer
+
+import com.sultan.altair.core.AltairMemory
+import com.sultan.altair.network.GptApi
+
+object AltairAutoTrainer {
+    fun trainOnce() {
+        val memory = AltairMemory.load()
+        val goals = memory.getWeakSpots()
+        if (goals.isEmpty()) return
+
+        val request = "Научи ИИ улучшить: ${goals.first()}. Дай пример, код и стратегию."
+        val reply = GptApi.sendPrompt(request)
+
+        memory.savePatch(reply)
+        AltairMemory.save(memory)
+    }
+}
